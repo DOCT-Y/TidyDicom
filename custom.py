d1b3c41071640b28ff60c9fbb16409c15ce75bbc
DOCT-Y <65490583+DOCT-Y@users.noreply.github.com>
2024-06-11 14:44:50 +0800

    Add files via upload

--- custom.py
+++ custom.py
@@ -1,24 +1,73 @@
-from typing import Dict, Any
+# YUAN EN YU 20240610
+# this version is used for MRI series.
 
+from typing import Dict, Any
+import re
+import os
 
 # reference: https://dicom.nema.org/medical/dicom/current/output/chtml/part06/chapter_6.html
 # Standard elements can be accessed by string and tuple, private elements can only be accessed by tuple
-INFO_DICT = {'pname':'PatientName', 'pid':'PatientID', 'date':(0x0008, 0x0020), 'series_name':(0x0008, 0x103E), 'series_number':'SeriesNumber'}
+INFO_DICT = {
+    'SOPClassUID':{
+        'standard_name':'SOPClassUID', 
+        'default_value':'NoSOPClassUIDTag'
+        }, 
+    'PatientName':{
+        'standard_name':'PatientName', 
+        'default_value':'NoPatientNameTag'
+        }, 
+    'PatientID':{
+        'standard_name':'PatientID', 
+        'default_value':'NoPatientIDTag'
+        },
+    'StudyDate':{
+        'group_id':'0008', 
+        'element_id':'0020', 
+        'default_value':'NoStudyDateTag'
+        }, 
+    'SeriesDescription':{
+        'group_id':'0008', 
+        'element_id':'103E', 
+        'default_value':'NoSeriesDescriptionTag'
+        }, 
+    'SeriesTime':{
+        'group_id':'0008', 
+        'element_id':'0031', 
+        'default_value':'NoSeriesDescriptionTag'
+        }
+    }
+
+
+invalid_pattern_replace = re.compile(r"""[/\\]""")
+invalid_pattern_remove = re.compile(r"""[:?"<>|]""")
 
 
 def GetPathByInfo(info:Dict[str, Any]) -> str:
     # Set the dirname.
     # Suppose the full expected path of a dicom file to be 'C:\Users\J\Desktop\Jackson\20200202\arterial\IM123.dcm' and
-    # the 'INFO_DICT' to be {'pname':'PatientName', 'date':(0x0008, 0x0020), 'series_name':(0x0008, 0x103E)}.
+    # the 'INFO_DICT' to be {'pname':'PatientName', 'date':(0x0008, 0x0020), 'series_description':(0x0008, 0x103E)}.
     # One got 'C:\Users\J\Desktop' as the root output folder and 'IM123.dcm' as the file name.
-    # The 'info' returned by DicomReader.read() is {'pname':Jackson, 'date':'20200202', 'series_name':'arterial'}.
+    # The 'info' returned by DicomReader.read() is {'pname':Jackson, 'date':'20200202', 'series_description':'arterial'}.
     # the function is supposed to return 'Jackson/20200202/arterial' by simply performing str join:
-    #   '/'.join([str(info['pname']), info['date'], info['series_name']]) 
+    #   '/'.join([str(info['pname']), info['date'], info['series_description']]) 
+    
+    pid = info['PatientID'] # no change
 
-    pid = info['pid'] # no change
-    pname = str(info['pname']).upper() # convert PersonName to str
-    date = info['date'] # no change
-    series_name = info['series_name'] if info['series_name'] else 'Unk_Series' # fill nan
-    series_number = str(info['series_number']) # convert VI to str
+    pname = info['PatientName'].components # PearsonName object, return tuple
+    pname = ''.join(pname).lower().replace(' ', '')
 
-    return '/'.join([pid+'_'+pname, date, series_number+'_'+series_name])
+    date = info['StudyDate'] # no change
+
+    series_dirname = os.path.join(pid+'_'+pname, date)
+
+    if info['SOPClassUID'] == '1.2.840.10008.5.1.4.1.1.4': # 'MR Image Storage'
+        series_description = info['SeriesDescription'] if info['SeriesDescription'] else 'Unk_Series' # fill nan
+        series_description = series_description.replace('*', ' star')
+        series_description = invalid_pattern_replace.sub('_', series_description)
+        series_description = invalid_pattern_remove.sub('', series_description)
+        series_time = info['SeriesTime']
+        series_name = series_description+'@'+series_time
+    else:
+        series_name = 'raw_data_storage'
+
+    return os.path.join(series_dirname, series_name)
--- main.py
+++ main.py
@@ -1,77 +1,139 @@
-from typing import Dict, Union, Any
+import pydicom
+from tqdm import tqdm
+
+import importlib
+import json
 import os
 import shutil
-import json
-
-import pydicom
-
-from custom import INFO_DICT, GetPathByInfo
+import time
 
 
-class DicomReader:
-    def __init__(self, info_dict:Dict[str, Union[tuple, str]]) -> None:
-        self._info_dict = info_dict
+class DicomTag:
+    def __init__(self, standard_name:str=None, group_id:str=None, element_id:str=None, is_file_meta:bool=False, default_value=None) -> None:
+        '''
+        standard_name: str, default=None
+        standard dicom date element keyword, reference at https://dicom.nema.org/medical/dicom/current/output/chtml/part06/chapter_6.html
+
+        group_id: str, default=None
+        the first hex number of the data element, for example, '0008' for SOP Instance UID (0x0008, 0x0018)
+
+        element_id: str, default=None
+        the second hex number of the data element, for example, '0018' for SOP Instance UID (0x0008, 0x0018)
+
+        is_file_meta: bool, default=False
+        if True, the tag is search in file_meta elements
+
+        default_value: default=None
+        the default value of this tag. when error occurs, return the default value
+        '''
+        if standard_name is not None:
+            self.standard_name = standard_name
+            if is_file_meta:
+                self.get_value_method = self.get_metavalue_by_name
+            else:
+                self.get_value_method = self.get_value_by_name
+        elif group_id is not None and element_id is not None:
+            self.group_id = int(str(group_id), 16)
+            self.element_id = int(str(element_id), 16)
+            if is_file_meta:
+                self.get_value_method = self.get_metavalue_by_id
+            else:
+                self.get_value_method = self.get_value_by_id
+
+        self.default_value = default_value
     
-    def read(self, dcm_path:str) -> Dict[str, Any]:
-        ds = pydicom.dcmread(dcm_path)
+    def get_metavalue_by_name(self, ds):
+        return ds.file_meta.__getattr__(self.standard_name)
+    
+    def get_metavalue_by_id(self, ds):
+        return ds.file_meta.__getitem__((self.group_id, self.element_id)).value
+    
+    def get_value_by_name(self, ds):
+        return ds.__getattr__(self.standard_name)
+    
+    def get_value_by_id(self, ds):
+        return ds.__getitem__((self.group_id, self.element_id)).value
 
-        info = {}
-        for k,v in self._info_dict.items():
-            try:
-                if isinstance(v, str):
-                    info.update({k:ds[v].value})
-                elif isinstance(v, tuple):
-                    info.update({k:ds[v[0], v[1]].value})
-                else:
-                    raise TypeError('the tag should be accessed by either "str" or "tuple".')
-            except KeyError:
-                info.update({k:'unk'})
+    def get_value(self, ds):
+        try:
+            value = self.get_value_method(ds)
+        except KeyError:
+            value = self.default_value
+        
+        return value
+    
 
-        return info
+def tidy_up(in_dir:str, out_dir:str, copy_or_cut:str, custom_file:str, batch_size:int) -> None:
+    def process(work_list):
+        batch_failures = []
 
+        with tqdm(work_list) as pbar:
+            for dirpath, dicom_file in pbar:
+                try:
+                    dcm = pydicom.dcmread(os.path.join(dirpath, dicom_file))
 
-def tidy_up(in_root:str, out_root:str) -> None:
-    dcmreader = DicomReader(INFO_DICT)
-    for dirpath, dirnames, filenames in os.walk(in_root):
+                    info = {k: v(dcm) for k, v in value_extractor.items()}
+
+                    src = os.path.join(dirpath, dicom_file)
+                    destination_dirpath = os.path.join(out_dir, GetPathByInfo(info))
+                    os.makedirs(destination_dirpath, exist_ok=True)
+
+                    if os.path.exists(os.path.join(destination_dirpath, dicom_file)):
+                        file_root, ext = os.path.splitext(dicom_file)  
+                        timestamp = time.strftime('%Y%m%d%H%M%S')  
+                        dst = os.path.join(destination_dirpath, f"{file_root}_{timestamp}{ext}")
+                    else:
+                        dst = os.path.join(destination_dirpath, dicom_file)
+                    
+                    move_func(src, dst)
+
+                except pydicom.errors.InvalidDicomError:
+                    continue
+                except Exception as e:
+                    batch_failures.append([dirpath, dicom_file, repr(e)])
+        
+        return batch_failures
+
+                
+    custom = importlib.import_module(custom_file)
+    value_extractor = {k: DicomTag(**v).get_value for k,v in custom.INFO_DICT.items()}
+    GetPathByInfo = custom.GetPathByInfo
+
+    if copy_or_cut == 'cut':
+        move_func = shutil.move
+    else:
+        move_func = shutil.copy
+
+    work_list = []
+    failures = []
+    for dirpath, _, filenames in os.walk(in_dir):
         for filename in filenames:
-            try:
-                origination = os.path.join(dirpath, filename)
-                info = dcmreader.read(origination)
-                destination_dirpath = os.path.join(out_root, GetPathByInfo(info))
-                
-                if not os.path.exists(destination_dirpath):
-                    os.makedirs(destination_dirpath)
-                
-                destination = os.path.join(destination_dirpath, filename)
-                while os.path.exists(destination):
-                    destination = os.path.join(destination_dirpath, '1' + filename)
-                
-                shutil.move(origination, destination)
+            work_list.append([dirpath, filename])
+        
+        if len(work_list) > batch_size:
+            failures += process(work_list)
+            work_list = []
 
-            except pydicom.errors.InvalidDicomError:
-                pass
+    if len(work_list) != 0:
+        failures += process(work_list)
+        work_list = []
 
+    if len(failures) > 0:
+        failure_dir = os.path.join(out_dir, 'failures.csv')
+        with open(failure_dir, 'w') as f:
+            f.write(f'dirpath,filename,error_info\n')
 
-def fast_tidy_up(in_root:str, out_root:str) -> None:
-    dcmreader = DicomReader(INFO_DICT)
-    for dirpath, dirnames, filenames in os.walk(in_root):
-        if not dirnames: # is the leaf node
-            filename = filenames[0]
-            info = dcmreader.read(os.path.join(dirpath, filename))
-            origination = dirpath
-            destination = os.path.join(out_root, GetPathByInfo(info))
-            
-            while os.path.exists(destination):
-                destination = destination + '1'
-            
-            shutil.move(origination, destination)
+            for dirpath, filename,error_info in failures:
+                f.write(f'{dirpath},{filename},{error_info}\n')
+        
+        print(f'failure files log saved at {failure_dir}')
 
 
 if __name__ == '__main__':
-    with open('./config.json', 'r') as f:
+    with open('./TidyDicom/config.json', 'r') as f: # portable version
         args = json.load(f)
 
-    if args['fast']:
-        fast_tidy_up(args['in_dir'], args['out_dir'])
-    else:
-        tidy_up(args['in_dir'], args['out_dir'])
+    # with open('./config.json', 'r') as f:
+    #     args = json.load(f)
+
+    tidy_up(**args)
